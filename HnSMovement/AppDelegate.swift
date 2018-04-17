8f755362d09b69a499e020acd1e798d8fa13ad07
Velin92 <maurovelin@yahoo.com>
2018-04-17 15:20:28 +0200

    Initial Commit

---
+++ HnSMovement/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  HnSMovement
+//
+//  Created by Mauro Romito on 17/04/18.
+//  Copyright © 2018 maurrandrea. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
